f532b4abcea1be5787a447ed22e0aaf81fa12631
jacarlianda <jacarlianda@gmail.com>
2019-09-28 23:56:17 +0800

    base framework

---
+++ src/main/java/cn/jacarlianda/controller/HelloController.java
@@ -0,0 +1,19 @@
+package cn.jacarlianda.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot 2.0!";
+    }
+
+    @RequestMapping("/code")
+    public String getCode() {
+        return "Hello";
+    }
+
+}
